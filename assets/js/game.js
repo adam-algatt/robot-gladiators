6c3d75fc3b72e9922443519109f745762584b68c
adam-algatt <adam.algatt@gmail.com>
2022-02-07 15:20:23 -0800

    added some JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,7 @@
-window.alert("This is an alert! JS is running!")
+let roboName = window.prompt("What is your robot's name?");
+function fight() {
+    roboName;
+    window.alert("Hold onto your socks " + roboName + " it's gonna get a little hairy");
+window.alert("The fight has begun!");
+}
+//fight(); 
